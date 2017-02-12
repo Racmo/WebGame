f7f53823249a41c1978f47c198187050726ae468
Racmo <monia.racek@gmail.com>
2017-02-12 14:18:46 +0100

    Add keyboard control

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+//class which stores information about keys status
+function Keys(){
+	this.upPressed = false;
+	this.leftPressed = false;
+	this.downPressed = false;
+	this.rightPressed = false;
+}
+
+//player class
+function Player(id){
+		this.id = id;
+		this.name = 'noName';
+		this.x = 10;
+		this.y = 10;
+		this.speed = 3;
+		this.keys = new Keys();
+
+		this.updatePosition = function(){
+			if(this.keys.upPressed) this.y-=this.speed;
+			if(this.keys.leftPressed) this.x-=this.speed;
+			if(this.keys.downPressed) this.y+=this.speed;
+			if(this.keys.rightPressed) this.x+=this.speed;
+		};
+}
+
+/////////////////////////////////////
 var express = require('express'); //import module
 var app = express(); //creates express aplication
 var serv = require('http').Server(app);
@@ -9,6 +35,7 @@ app.get('/', function(req, res){
 
 app.use('/client', express.static(__dirname + '/client')); //access only files from client directory?
 
+//get script.js which works on index.html
 app.get('/js/script.js', function(req, res){
 	console.log('zapytanie o skrypt');
 	res.sendFile(__dirname + '/client/js/script.js');
@@ -16,3 +43,70 @@ app.get('/js/script.js', function(req, res){
 
 
 serv.listen(2000);
+/////////////////////////////////////
+
+var connectionNumber = 0; 
+var connections = []; //array with connected sockets
+var players = []; //array with players
+
+var io = require('socket.io')(serv, {}); //import module
+
+//when someone connects to server 
+io.sockets.on('connection', function(socket){
+	console.log('connected');
+	socket.id = connectionNumber;
+	connectionNumber++;
+
+	player = new Player(socket.id);
+
+	players[socket.id] = player;
+	connections[socket.id] = socket;
+
+	socket.on('key pressed', function(data){
+		if(data.direction === 'up')
+			players[socket.id].keys.upPressed = true;
+		if(data.direction === 'down')
+			players[socket.id].keys.downPressed = true;
+		if(data.direction === 'left')
+			players[socket.id].keys.leftPressed = true;
+		if(data.direction === 'right')
+			players[socket.id].keys.rightPressed = true;
+	});
+	socket.on('key released', function(data){
+		if(data.direction === 'up')
+			players[socket.id].keys.upPressed = false;
+		if(data.direction === 'down')
+			players[socket.id].keys.downPressed = false;
+		if(data.direction === 'left')
+			players[socket.id].keys.leftPressed = false;
+		if(data.direction === 'right')
+			players[socket.id].keys.rightPressed = false;
+	});
+
+	//listen to disconnect event
+	socket.on('disconnect',function(){
+		delete connections[socket.id];
+		delete players[socket.id];
+	});
+
+});
+
+//update positions and send position data to connected players every 50ms
+setInterval(function(){
+
+	var newPositions = [];
+
+	for(var i in players){
+		players[i].updatePosition();
+
+		newPositions.push({
+			x: players[i].x,
+			y: players[i].y
+		});
+	}
+
+	for(var i in connections){
+		//send data about all connected players to all connected players
+		connections[i].emit('position',newPositions);
+	}
+}, 50);
--- client/js/script.js
+++ client/js/script.js
@@ -1,5 +1,48 @@
-console.log('ja dzialam');
+
 var canvas = document.getElementById('game').getContext('2d');
 canvas.font = '30px Arial';
 
-canvas.fillText('Hello!', 30, 30);
+var socket = io();
+
+//get positions from server and paint them on canvas
+socket.on('position', function(positions){
+	canvas.clearRect(0,0,600,400);
+	for(var i in positions){
+		canvas.fillText('X', positions[i].x, positions[i].y);
+	}
+	
+});
+
+//send info about pressed keys
+document.onkeydown = function(event){
+	console.log('key pressed');
+	//up
+	if(event.keyCode === 38 || event.keyCode === 87) 
+		socket.emit('key pressed', {direction: 'up'})
+	//down
+	if(event.keyCode === 40 || event.keyCode === 83)
+		socket.emit('key pressed', {direction: 'down'})
+	//left
+	if(event.keyCode === 37 || event.keyCode === 65)
+		socket.emit('key pressed', {direction: 'left'})
+	//right
+	if(event.keyCode === 39 || event.keyCode === 68)  
+		socket.emit('key pressed', {direction: 'right'})
+}
+
+//send info about key release
+document.onkeyup = function(event){
+	console.log('key released');
+	//up
+	if(event.keyCode === 38 || event.keyCode === 87) 
+		socket.emit('key released', {direction: 'up'})
+	//down
+	if(event.keyCode === 40 || event.keyCode === 83)
+		socket.emit('key released', {direction: 'down'})
+	//left
+	if(event.keyCode === 37 || event.keyCode === 65)
+		socket.emit('key released', {direction: 'left'})
+	//right
+	if(event.keyCode === 39 || event.keyCode === 68)  
+		socket.emit('key released', {direction: 'right'})
+}
