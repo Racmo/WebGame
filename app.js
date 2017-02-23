0b2dfd1c94ebd7a85f053f8a520651a27a7c78cb
Racmo <monia.racek@gmail.com>
2017-02-23 19:37:39 +0100

    Use phaser library

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+CANVAS_WIDTH = 800;
+CANVAS_HEIGHT = 500;
+AVATAR_WIDTH = 64;
+AVATAR_HEIGHT = 64;
+
 //class which stores information about keys status
 function Keys(){
 	this.upPressed = false;
@@ -8,19 +13,24 @@ function Keys(){
 
 //player class
 function Player(id){
-		this.id = id;
-		this.name = 'noName';
-		this.x = 10;
-		this.y = 10;
-		this.speed = 3;
-		this.keys = new Keys();
+	this.id = id;
+	this.name = 'noName';
+	this.x = 10;
+	this.y = 10;
+	this.speed = 4;
+	this.keys = new Keys();
 
-		this.updatePosition = function(){
-			if(this.keys.upPressed) this.y-=this.speed;
-			if(this.keys.leftPressed) this.x-=this.speed;
-			if(this.keys.downPressed) this.y+=this.speed;
-			if(this.keys.rightPressed) this.x+=this.speed;
-		};
+	this.updatePosition = function(){
+		if(this.keys.upPressed) this.y-=this.speed;
+		if(this.keys.leftPressed) this.x-=this.speed;
+		if(this.keys.downPressed) this.y+=this.speed;
+		if(this.keys.rightPressed) this.x+=this.speed;
+
+		if(this.x + AVATAR_WIDTH> CANVAS_WIDTH) this.x = CANVAS_WIDTH - AVATAR_WIDTH;
+		if(this.y + AVATAR_HEIGHT > CANVAS_HEIGHT) this.y = CANVAS_HEIGHT - AVATAR_HEIGHT;
+		if(this.x < 0) this.x = 0;
+		if(this.y < 0) this.y = 0;
+	};
 }
 
 /////////////////////////////////////
@@ -41,8 +51,13 @@ app.get('/js/script.js', function(req, res){
 	res.sendFile(__dirname + '/client/js/script.js');
 });
 
-app.get('/img/pusheen.png', function(req, res){
-	res.sendFile(__dirname + '/client/img/pusheen.png');
+app.get('/img/Lumber1.png', function(req, res){
+	console.log('send lubmer');
+	res.sendFile(__dirname + '/client/img/Lumber1.png');
+});
+
+app.get('/js/socket.js', function(req, res){
+	res.sendFile(__dirname + '/client/js/socket.js');
 });
 
 serv.listen(2000);
@@ -65,31 +80,7 @@ io.sockets.on('connection', function(socket){
 	players[socket.id] = player;
 	connections[socket.id] = socket;
 
-	socket.on('key pressed', function(data){
-		if(data.direction === 'up')
-			players[socket.id].keys.upPressed = true;
-		if(data.direction === 'down')
-			players[socket.id].keys.downPressed = true;
-		if(data.direction === 'left')
-			players[socket.id].keys.leftPressed = true;
-		if(data.direction === 'right')
-			players[socket.id].keys.rightPressed = true;
-	});
-	socket.on('key released', function(data){
-		if(data.direction === 'up')
-			players[socket.id].keys.upPressed = false;
-		if(data.direction === 'down')
-			players[socket.id].keys.downPressed = false;
-		if(data.direction === 'left')
-			players[socket.id].keys.leftPressed = false;
-		if(data.direction === 'right')
-			players[socket.id].keys.rightPressed = false;
-	});
-
-	socket.on('change name', function(data){
-		console.log(data.name)
-		players[socket.id].name = data.name;
-	});
+	socket.emit('yourId', socket.id); //send back id to player
 
 	//listen to disconnect event
 	socket.on('disconnect',function(){
@@ -97,25 +88,58 @@ io.sockets.on('connection', function(socket){
 		delete players[socket.id];
 	});
 
+	//after game is started
+	socket.on('gameStarted', function(){
+		console.log('gameStarted');
+
+		socket.on('key pressed', function(data){
+			console.log('button pushed');
+			if(data.direction === 'up')
+				players[socket.id].keys.upPressed = true;
+			if(data.direction === 'down')
+				players[socket.id].keys.downPressed = true;
+			if(data.direction === 'left')
+				players[socket.id].keys.leftPressed = true;
+			if(data.direction === 'right')
+				players[socket.id].keys.rightPressed = true;
+		});
+		socket.on('key released', function(data){
+			console.log('button released');
+			if(data.direction === 'up')
+				players[socket.id].keys.upPressed = false;
+			if(data.direction === 'down')
+				players[socket.id].keys.downPressed = false;
+			if(data.direction === 'left')
+				players[socket.id].keys.leftPressed = false;
+			if(data.direction === 'right')
+				players[socket.id].keys.rightPressed = false;
+		});
+
+		socket.on('change name', function(data){
+			console.log(data.name)
+			players[socket.id].name = data.name;
+		});
+
+		setInterval(function(){
+			var newPositions = [];
+
+			for(var i in players){
+				console.log(i);
+				players[i].updatePosition();
+
+				newPositions.push({
+					id: players[i].id,
+					name: players[i].name,
+					x: players[i].x,
+					y: players[i].y
+				});
+			}
+			if(newPositions != null)
+				socket.emit('position', newPositions);
+			console.log(newPositions);
+		}, 50);
+
+	});
+
 });
 
-//update positions and send position data to connected players every 50ms
-setInterval(function(){
-
-	var newPositions = [];
-
-	for(var i in players){
-		players[i].updatePosition();
-
-		newPositions.push({
-			name: players[i].name,
-			x: players[i].x,
-			y: players[i].y
-		});
-	}
-
-	for(var i in connections){
-		//send data about all connected players to all connected players
-		connections[i].emit('position',newPositions);
-	}
-}, 50);
--- client/js/script.js
+++ client/js/script.js
@@ -1,71 +1,120 @@
-
-var canvas = document.getElementById('game').getContext('2d');
-canvas.font = '15px Arial';
+var players = []; //array with connected players
+var myId; 
 
 var socket = io();
-//player character img
-var avatar = new Image();
-avatar.src = '../img/pusheen.png';
-
-//get positions from server and paint them on canvas
-socket.on('position', function(positions){
-	canvas.clearRect(0,0,800,500);
-	for(var i in positions){
-		canvas.fillText(positions[i].name, positions[i].x, positions[i].y+50);
-		canvas.drawImage(avatar, positions[i].x, positions[i]. y, 60, 40);
-	}
-	
+socket.on('yourId', function(id){
+	myId = id;
+	console.log(myId);
 });
 
-//send info about pressed keys
-document.onkeydown = function(event){
-	console.log('key pressed');
-	//up
-	if(event.keyCode === 38 || event.keyCode === 87) 
-		socket.emit('key pressed', {direction: 'up'})
-	//down
-	if(event.keyCode === 40 || event.keyCode === 83)
-		socket.emit('key pressed', {direction: 'down'})
-	//left
-	if(event.keyCode === 37 || event.keyCode === 65)
-		socket.emit('key pressed', {direction: 'left'})
-	//right
-	if(event.keyCode === 39 || event.keyCode === 68)  
-		socket.emit('key pressed', {direction: 'right'})
+//player class
+function Player(id, name){
+	this.id = id;
+	this.name = name;
+
+	this.sprite = game.add.sprite(0,0, 'lumber');
+
+	console.log('New player created >>> name: '+ this.name + ' id: ' + this.id);
+	players.push(this); //push player to players array
+
+	this.updateSpritePosition = function(x, y){
+		this.sprite.x = x;
+		this.sprite.y = y;
+	};
+} 
+
+//checks if player is on list
+function playerOnList(searchedId, list){
+	console.log('searchedId'+searchedId);
+	for(var i in list){
+		if(searchedId === list[i].id){
+			console.log('index '+i);
+			return i;
+		}
+	}
+	return -1;
 }
 
+function updatePlayerPositions(newPositions){
+	for(var pos in newPositions){
+		console.log('newPosition id: '+newPositions[pos].id);
+		var indexOfPlayer = playerOnList(newPositions[pos].id, players);
+		if(indexOfPlayer >= 0){
+			//player is already on the list of connected players
+			players[indexOfPlayer].updateSpritePosition(newPositions[pos].x, newPositions[pos].y);
+		} else {
+			//create new player
+			new Player(newPositions[pos].id, newPositions[pos].name);
+		}
+	}
+}
+
+function manageConnection(){
+	socket.emit('gameStarted');
+	console.log('gameStarted sent');
+
+	socket.on('position', function(positions){
+		for(var i in positions){
+			updatePlayerPositions(positions);
+		}
+	});
+}
+
+
+function keyboardInput(){
+
+	document.onkeydown = function(event){
+		console.log('key pressed');
+		//up
+		if(event.keyCode === 38 || event.keyCode === 87) 
+			socket.emit('key pressed', {direction: 'up'})
+		//down
+		if(event.keyCode === 40 || event.keyCode === 83)
+			socket.emit('key pressed', {direction: 'down'})
+		//left
+		if(event.keyCode === 37 || event.keyCode === 65)
+			socket.emit('key pressed', {direction: 'left'})
+		//right
+		if(event.keyCode === 39 || event.keyCode === 68)  
+			socket.emit('key pressed', {direction: 'right'})
+	}
+	
 //send info about key release
-document.onkeyup = function(event){
-	console.log('key released');
-	//up
-	if(event.keyCode === 38 || event.keyCode === 87) 
-		socket.emit('key released', {direction: 'up'})
-	//down
-	if(event.keyCode === 40 || event.keyCode === 83)
-		socket.emit('key released', {direction: 'down'})
-	//left
-	if(event.keyCode === 37 || event.keyCode === 65)
-		socket.emit('key released', {direction: 'left'})
-	//right
-	if(event.keyCode === 39 || event.keyCode === 68)  
-		socket.emit('key released', {direction: 'right'})
+	document.onkeyup = function(event){
+		console.log('key released');
+		//up
+		if(event.keyCode === 38 || event.keyCode === 87) 
+			socket.emit('key released', {direction: 'up'})
+		//down
+		if(event.keyCode === 40 || event.keyCode === 83)
+			socket.emit('key released', {direction: 'down'})
+		//left
+		if(event.keyCode === 37 || event.keyCode === 65)
+			socket.emit('key released', {direction: 'left'})
+		//right
+		if(event.keyCode === 39 || event.keyCode === 68)  
+			socket.emit('key released', {direction: 'right'})
+	}
 }
 
-function submitName(){
-	console.log('change name');
+//Phaser
 
-	var name = document.getElementById('nameText').value;
-	socket.emit('change name', {name: name});
+var game = new Phaser.Game(800, 500, Phaser.AUTO, "#game-container");
+var gameState = {
+	//load assets
+	preload: function(){
+		this.load.image('lumber', '../img/Lumber1.png');
+	},
+	//executed after everything is loaded
+	create: function(){
+		manageConnection();
+		keyboardInput();
+	},
+	//executed multiple times per second
+	update: function(){
 
-	return false; //to prevent page reloading on button click
+	}
 }
 
-
-///////////////////////////////////////////////////////////////
-window.onbeforeunload = function(){
-   socket.close();
-}
-//for opera
-window.addEventListener("beforeunload", function(e){
-   socket.close();
-}, false);
+game.state.add('GameState', gameState);
+game.state.start('GameState');
